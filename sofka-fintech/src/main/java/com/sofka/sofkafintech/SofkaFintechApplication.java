ae315401f8ccb35d32f393b3e2ac530855bb6631
braq12 <36553982+braq12@users.noreply.github.com>
2026-01-02 15:41:10 -0500

    Proyecto inicial

---
+++ sofka-fintech/src/main/java/com/sofka/sofkafintech/SofkaFintechApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.sofkafintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofkaFintechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofkaFintechApplication.class, args);
+	}
+
+}
---
+++ sofka-fintech/src/test/java/com/sofka/sofkafintech/SofkaFintechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.sofkafintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofkaFintechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
